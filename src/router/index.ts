9ab39cd22b7e6a73816f28e5339bee26ffa65cc4
andijuhan <80870917+andijuhan@users.noreply.github.com>
2023-06-20 20:53:05 +0700

    update

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import userRouter from './userRouter';
 
 const router = express.Router();
 
 export default (): express.Router => {
+   userRouter(router);
    return router;
 };
